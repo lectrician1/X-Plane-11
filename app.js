28e128cfdcde8148c9802d2ba8b44828966b7194
Seth Deegan <jayandseth@gmail.com>
2018-07-08 12:07:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,18 +15,17 @@ client.on('message', msg => {
 });
 
 request('https://www.x-plane.com/blog/', function (error, response, body) {
-  console.log('Request error:', error); // Print the error if one occurred
-  console.log('Request statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('Request body:', body); // Print the HTML for the Google homepage.
-});
-
-jsdom.env({
-  html: body,
-  scripts: ['https://code.jquery.com/jquery-3.3.1.min.js']
-}, function(err, window) {
-  //Use jQuery just as in a regular HTML page
-  const $ = window.jQuery;
-  console.log($('title').text());
+  console.log('Request error:', error);
+  console.log('Request statusCode:', response && response.statusCode); 
+  // console.log('Request body:', body);
+  jsdom.env({
+    html: body,
+    scripts: ['https://code.jquery.com/jquery-3.3.1.min.js']
+  }, function(err, window) {
+    //Use jQuery just as in a regular HTML page
+    const $ = window.jQuery;
+    console.log($('title').text());
+  });
 });
 
 client.login(process.env.token);
