bc9fa185b5d2f6743f8be37d49943bde1e21f15a
Seth Deegan <jayandseth@gmail.com>
2018-11-25 14:40:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ client.on('message', msg => {
         }
         else msg.reply('The only availible property for \`add\` is \`role(s)\`');
       }
-      else if (msgMatch[1] === 'undefined') msg.reply('That is not an availible property for \`XP\`');
-      else if (msgMatch[1].startsWith('(') && msgMatch[1].endsWith(')')) msg.reply('That is not an availible selector for \`XP\`');
+      else if (msg.content.startsWith('XP.')) msg.reply('That is not an availible property for \`XP\`');
+      else if (typeof msgMatch[1] === 'defined' && msgMatch[1].startsWith('(') && msgMatch[1].endsWith(')')) msg.reply('That is not an availible selector for \`XP\`');
     }
   }
 });
